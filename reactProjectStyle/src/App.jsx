3231a6644e97bd3d1980e5a6818a19571ac04723
Marco Contreras <mcontreras42@outlook.com>
2025-05-10 16:22:00 -0400

    subiendo creacion componente estilizado

--- reactProjectStyle/src/App.jsx
+++ reactProjectStyle/src/App.jsx
@@ -1,16 +1,18 @@
 //import {Card} from "./components/Card/Card";
-import {Button} from "./components/Card/Button/Button"
-import {Button2} from "./components/ButonestilizadoNuevo/ButtonConNewStyle"
+//import {Button} from "./components/Card/Button/Button"
+//import {Button2} from "./components/ButonestilizadoNuevo/ButtonConNewStyle"
+import {TailwindText} from "./components/text/HolaMundo";
 
 function App() {
   
   return (
     <>
-      <Button2 gris><p>Azul profundo</p></Button2>
-      <Button2 red><p>Azul profundos</p></Button2>
-      <Button Gris><p>Azul</p></Button>
-      <Button blue><p>rojo</p></Button>
-     {/*<Card/>*/}
+      <TailwindText/>
+      {/*/ <Button2 gris><p>Azul profundo</p></Button2>*/}
+      {/*/<Button2 red><p>Azul profundos</p></Button2>*/}
+      {/*/<Button Gris><p>Azul</p></Button>*/}
+      {/*/<Button blue><p>rojo</p></Button>*/}
+      {/*<Card/>*/}
     </>
   )
 }
